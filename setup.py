f919b9f23a6d2409d9a4b0e45c24c1018e02ca85
PearlOnyx08 <93891242+PearlOnyx08@users.noreply.github.com>
2021-11-07 21:05:11 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
 
 setup(
-    name="my_package",
-    entry_points={
-        "console_scripts": ["spacy_ann=spacy_ann.cli:main"],
-        "spacy_factories": ["ann_linker=spacy_ann.ann_linker:AnnLinker"],
-        "spacy_kb": ["get_candidates=spacy_ann:get_candidates"],
-    },
+    name='al3',
+    version='0.1',
+    py_modules=['yourscript'],
+    entry_points='''
+        [console_scripts]
+        spacy_ann=spacy_ann.cli:main
+
+        [spacy_factories]
+        ann_linker=spacy_ann.ann_linker:AnnLinker
+        remote_ann_linker=spacy_ann.remote_ann_linker:RemoteAnnLinker
+
+        [spacy_kb]
+        get_candidates=spacy_ann:get_candidates
+    ''',
 )
