38f74910d945ddd64f2150cdbe28b222f34f8ff4
PearlOnyx08 <93891242+PearlOnyx08@users.noreply.github.com>
2021-11-12 14:14:45 -0600

    Delete spacy_ann directory

--- spacy_ann/__init__.py
+++
@@ -1,9 +0,0 @@
-
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-"""spaCy ANN Linker, a pipeline component for generating spaCy KnowledgeBase Alias Candidates for Entity Linking."""
-
-
-__version__ = "0.4.0"
-
-from .ann_linker import AnnLinker
